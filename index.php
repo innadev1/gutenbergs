a9e1653a938f76c2bc4b4571c4c64c2a8ab19b6b
innadev1 <innad2201@gmail.com>
2017-10-13 16:53:48 +0300

    ew

--- index.php
+++ index.php
@@ -69,26 +69,192 @@
 	}
 ?>
 
+<?php
+
+$error_message_df = "";
+$error_message_dt = "";
+$error_message_no = "";
+$error_message_peo = "";
+$error_message_peo2 = "";
+$error_message_rooms = "";
+$error_message_rooms2 = "";
 
 
 
+
+if(isset($_GET['submit_two'])) {
+	$to = 'my.worktest94@gmail.com';
+
+	$error_message_df = "";
+	$error_message_dt = "";
+	$error_message_no = "";
+	$error_message_peo = "";
+	$error_message_peo2 = "";
+	$error_message_rooms = "";
+	$error_message_rooms2 = "";
+
+	$check_in_date = $_GET["check_in_date"];
+	$check_out_date = $_GET["check_out_date"];
+	$adults = $_GET["number_adults"];
+	$childrens = $_GET["number_children"];
+
+	$errors = ['check_in_date'=>0,'check_out_date'=>0, 'number_adults'=>0, 'number_children'=>0];
+
+	// Date
+	if(empty($check_in_date)) {
+		$error_message_df .= '<p style = "color: red;">Please enter date.</p>';
+		$errors['check_in_date'] = 1;
+	}
+
+	if(empty($check_out_date)) {
+		$error_message_dt .= '<p style = "color: red;">Please enter date.</p>';
+		$errors['check_out_date'] = 1;
+	}
+
+	if($check_in_date > $check_out_date ){
+		$error_message_no .= '<p style = "color: red;">Going back in time impossible.</p>';
+		$errors['check_in_date'] = 1;
+		$errors['check_out_date'] = 1;
+	}
+
+	// adults
+	if(strlen($adults) < 1){
+		$error_message_peo .= '<p style = "color: red;">Enter number of peoples.</p>';
+		$errors['number_adults'] = 1;
+	}
+
+	$error_message = "";
+	$email_exp_n = "/[^0-9]/";
+
+	if(preg_match($email_exp_n,$_GET["number_adults"])) {
+		$error_message_peo2 .= '<p style = "color: red;">only numbers!</p>';
+		$errors['number_adults'] = 1;
+	}
+
+	// childrens
+	if(strlen($childrens) < 1){
+		$error_message_rooms .= '<p style = "color: red;">Enter number of peoples.</p>';
+		$errors['number_children'] = 1;
+	}
+
+	$error_message = "";
+	$email_exp_n = "/[^0-9]/";
+
+	if(preg_match($email_exp_n,$_GET["number_children"])) {
+		$error_message_rooms2 .= '<p style = "color: red;">only numbers!</p>';
+		$errors['number_children'] = 1;
+	}
+
+}
+
+?>
 
 <html>
     <head>     
-		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;subset=cyrillic" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&amp;subset=cyrillic" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="style/index.css">
 		<link rel="stylesheet" type="text/css" href="style/style.css">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
         <title>gutenberg</title>
 		
-		<script type="text/javascript" src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel = "stylesheet">
+      	<script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
+      	<script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+		<!-- Javascript -->
+		<script>
+         	$(function() {
+            	$( "#datepicker-10" ).datepicker({
+					minDate: 0,
+					changeMonth:true,
+					changeYear:true,
+					numberOfMonths:[1,1],
+					dateFormat: "yy-mm-dd"
+            	});
+         	});
+      	</script>
+		<script>
+         	$(function() {
+            	$( "#datepicker-11" ).datepicker({
+					minDate: 0,
+					changeMonth:true,
+					changeYear:true,
+					numberOfMonths:[1,1],
+					dateFormat: "yy-mm-dd"
+            	});
+         	});
+      	</script>
+		<script>
+         	$(function() {
+            	$( "#datepicker-12" ).datepicker({
+					minDate: 0,
+					changeMonth:true,
+					changeYear:true,
+					numberOfMonths:[1,1],
+					dateFormat: "yy-mm-dd"
+            	});
+         	});
+      	</script>
+		<script>
+         	$(function() {
+            	$( "#datepicker-13" ).datepicker({
+					minDate: 0,
+					changeMonth:true,
+					changeYear:true,
+					numberOfMonths:[1,1],
+					dateFormat: "yy-mm-dd"
+            	});
+         	});
+      	</script>
 
     </head>
 
 	<body>
 	
 		<?php include 'assets/header.php'; ?>
+	
+		<form id="form" name="orderform" method="get" action="index.php">
+			<div id="checkout">
+			
+				<div class="checkout">
+					<label><h1>Check in</h1></label><span class="wpcf7-form-control-wrap date-87"><input class="wpcf7-date" value = "<?php if(isset($_GET['check_in_date']) && $errors['check_in_date'] == 0 ){ echo $_GET['check_in_date']; } ?>" name="check_in_date" type = "text" readonly="readonly" id = "datepicker-10"></spam>
+					<?php echo ($error_message_df); ?>
+					<?php echo ($error_message_no); ?>
+				</div>
+
+				<div class="checkout">
+					<label><h1>Check out</h1></label><span class="wpcf7-form-control-wrap date-87"><input class="wpcf7-date" value = "<?php if(isset($_GET['check_out_date']) && $errors['check_out_date'] == 0){ echo $_GET['check_out_date']; } ?>" name="check_out_date" type = "text" readonly="readonly" id = "datepicker-11"></spam>
+					<?php echo ($error_message_dt); ?>
+					<?php echo ($error_message_no); ?>
+				</div>
+
+
+				<div class="checkout">
+					<a><h1>Adults</h1><span class="inputs"><input type="text" value = "<?php if(isset($_GET['number_adults']) && $errors['number_adults'] == 0){ echo $_GET['number_adults']; } ?>" name="number_adults" class="wpcf7-date"></span></a>
+					<?php echo ($error_message_peo); ?>
+					<?php echo ($error_message_peo2); ?>
+				</div>
+
+
+				<div class="checkout">
+					<a><h1>Children</h1><span class="inputs"><input type="text" value = "<?php if(isset($_GET['number_children']) && $errors['number_children'] == 0){ echo $_GET['number_children']; } ?>" name="number_children" class="wpcf7-date"></span></a>
+					<?php echo ($error_message_rooms); ?>
+					<?php echo ($error_message_rooms2); ?>
+				</div>
+
+
+				<div class="checkout" id="button" style="padding:0;">
+
+				
+				<?php
+					if(!empty($_GET)){
+
+						header("Location:https://app.thebookingbutton.com/properties/hgutenbergsdirect?locale=en&check_in_date=".$_GET['check_in_date']."&check_out_date=".$_GET['check_out_date']."&number_adults=".$_GET['number_adults']."&number_children=".$_GET['number_children']);
+				
+					}
+				?>
+					<input class="blackbutton" type="submit" id="submit" name="submit_two" value="book">
+				</div>
+			</div>
+		</form>
 	
 		<div class="header">
 			<img src="img/1.png">
@@ -99,7 +265,7 @@
 		<div class="wrap" id="first">
 			<div class="b-1">
 				<div><img src="img/l1.png"></div>
-				<h3>perfect view</h3>
+				<h3>hotel</h3>
 			</div>
 			<div class="b-2">
 				<div><img src="img/l2.png"></div>
---
+++ rooms.php
@@ -0,0 +1,49 @@
+<html>
+    <head>     
+		<link rel="stylesheet" type="text/css" href="style/rooms.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <title>rooms</title>
+    </head>
+
+	<body>
+	
+		<?php include 'assets/header.php'; ?>
+	
+		<div id="main">
+			<div class="main">
+				<div class="bed1"></div>
+				<div class="cont">
+					<div>
+						<h1>Standard room <br> single/double</h1>
+						<p>With a king size (220 x 180) bed, 2 bedside tables, a stand-lamp, a breakfast-table, 2 soft armchairs, a writing cabinet with a lamp, TV with international channels, a phone, Free WIFI Internet, a mini-bar. Twin room with two separate beds is available. In the bathroom – a shower or a bath at your option, a hair dryer, one power outlet, articles of personal hygiene - soap, shower gels, shower caps, 3 towels, but during summer also flowers. Room service, laundry service, ironing, car services, etc.</p>
+						<button>book</button>
+					</div>
+				</div>
+			</div>
+			<div class="main">
+				<div class="bed2"></div>
+				<div class="cont">
+					<div>
+						<h1>Junior Suite <br> apartments</h1>
+						<p>Single or double apartments with a comfortable, furnished anteroom that would fit for a small meeting of 4-6 people or a child's bedroom. With one or two king size beds, 2 bedside tables, a ceiling lamp, a breakfast-table, 2 soft armchairs, a writing cabinet with a lamp, TV with international channels, a phone, Free WiFi, a mini-bar, in the bathroom – heated tile floor. More conveniences – the same as before.</p>
+						<button>book</button></div>
+					</div>
+			</div>
+			<div class="main">
+				<div class="bed3"></div>
+				<div class="cont">
+					<div>
+						<h1>De Luxe apartment</h1>
+						<p>Single or double De Luxe apartment with a comfortable anteroom with a guest restroom. Further on from the anteroom – living room of 25 m2 with 4 windows, one opens to the very floor and a rocking-chair by the large TV-set. The living room has a comfortable writing cabinet, a chest of drawers, a mini-bar. Behind the bedroom – the second spacious restroom with a bath and heated floor. The living room can host a small party of 6-10 people or serve as a children's bedroom. In the bedroom – a TV, the large king size bed. More conveniences – the same as before.</p>
+						<button>book</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<?php include 'assets/footer.php'; ?>
+		
+	</body>
+</html>
