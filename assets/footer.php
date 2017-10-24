301797f8ae9c7acbb09c184112650db1057f4aa2
WillSylvan <silvis.silvestrs@gmail.com>
2017-10-24 16:35:56 +0300

    done lang

--- assets/footer.php
+++ assets/footer.php
@@ -22,7 +22,8 @@
 			<h3><?php echo $language[$lang]['latest'] ?></h3>
 			<div class="c wraping">
 				<!--<div><img src="img/tw.png"></div>-->
-				<div><a><img src="img/f.png"></a></div>
+				<div><a href="https://www.facebook.com/gutenbergsterase"><img src="img/f.png"></a></div>
+				<div><a href="https://www.instagram.com/gutenbergs_riga/"><img src="img/in.png"></a></div>
 				<!--<div><img src="img/y.png"></div>-->
 			</div>
 		</div>
@@ -37,8 +38,31 @@
 				<a href="http://www.gutenbergs-apartment.lv/">
 					<img src="img/logo2.png" alt="HTML tutorial" style="width:42px;height:42px;border:0;">
 				</a>
+				
+				<a href="https://www.tripadvisor.co.uk/Hotel_Review-g274967-d283717-Reviews-Hotel_Gutenbergs-Riga_Riga_Region.html">
+					<img src="img/tripadvisor.png" alt="HTML tutorial" style="width:42px;height:42px;border:0;">
+				</a>
 
 			</div>
+		</div>
+		
+		<div class="footer_links">
+		
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10';
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+
+			<div class="fb-page" 
+			  data-href="https://www.facebook.com/gutenbergsterase"
+			  data-width="280" 
+			  data-hide-cover="false"
+			  data-show-facepile="false"></div>
+
 		</div>
 	</div>
 </footer>
